885c08d67c84a62d7514b5e6604419589142bbee
BuildTools <unconfigured@null.spigotmc.org>
2022-08-29 16:37:20 -0400

    commit

--- Python Intro/program_3.py
+++ Python Intro/program_3.py
@@ -1,18 +1,19 @@
 def combine_dicts(dict1, dict2):
-    for x in dict1:
-        if x in dict2:
-            dict1[x] = dict2[x] + dict1[x]
-        else:
-            dict1[x]=dict2[x]
-            
-    return dict1
+    dict3 = {}
+    print(dict1, dict2)
+    for x in dict1.items():
+        for y in dict2.items():
+            if x[0]==y[0]:
+                dict3[x[0]]=x[1]+y[1]
+            else:
+                dict3[x[0]]=x[1]
+                dict3[y[0]]=y[1]
+    return dict3
 
-# dict1 = input("Input first dictionary: ")
-# dict2 = input("Input second dictionary: ")
-
-dict1 = {'a':100, 'b':200}
-dict2 = {'a':300, 'c':100}
+dict1 = eval(input("Input first dictionary: "))
+dict2 = eval(input("Input second dictionary: "))
 
 combined_dict = combine_dicts(dict1, dict2)
 
 print(combined_dict)
+
