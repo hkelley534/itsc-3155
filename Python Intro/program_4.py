f4ae082ff21d5c161df6b7f542cf8fca45be40a4
BuildTools <unconfigured@null.spigotmc.org>
2022-08-31 15:37:00 -0400

    commit

--- Python Intro/program_4.py
+++ Python Intro/program_4.py
@@ -1,13 +1,14 @@
 
 def sort_invoice(invoice):
     sorted_invoice = []
-    for x in range(len(invoice)):
-        sorted_invoice.append(invoice[x].split(" ")[1])
+    for item in invoice:
+        sorted_invoice.append(x)
+        print()
         
 
 item_num = int(input("Number of items: "))
 invoice = []
 for x in range(item_num):
     invoice.append(input("Input item and price: "))
-
+print(invoice)
 sort_invoice(invoice)
